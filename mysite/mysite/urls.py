5e8fa533bd8e6581e05daf8e5f402207a235a807
Gabriel Andrade Pinto <gabrielandrade.colorado@gmail.com>
2026-03-13 11:22:47 -0300

    feat: creating django-admin

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.urls import path
 from django.urls import path, include
+from django.contrib import admin
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path('home', include('blog.urls'))
 ]
